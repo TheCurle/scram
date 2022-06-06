ce71e941f7cb42e67bb7b33ab709304354ab705a
Curle <curle@gemwire.uk>
2022-06-06 02:43:00 +0100

    Move search bar to sidebar, set current open page.

--- src/main/java/uk/gemwire/scram/user/UserRepository.java
+++ src/main/java/uk/gemwire/scram/user/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Allows accessing AND modifying users through a JpaRepository.
  */
 public interface UserRepository extends JpaRepository<UserData, Long> {
-    UserData findByEmail(String email);
     UserData findByName(String username);
 
     @Override
